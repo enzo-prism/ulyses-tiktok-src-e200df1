dbbe56f57b4b38b5e5b372f3742c4862cf5759f6
enzo <enzo@design-prism.com>
2026-08-27 16:33:01 -0700

    Add views and sidebar barrels

---
+++ src/components/ui/sidebar.tsx
@@ -0,0 +1,29 @@
+export {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarInput,
+  SidebarInset,
+  SidebarProvider,
+  SidebarRail,
+  SidebarSeparator,
+  SidebarTrigger,
+  useSidebar,
+} from "@/components/ui/sidebar-core"
+
+export {
+  SidebarGroup,
+  SidebarGroupAction,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuAction,
+  SidebarMenuBadge,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSkeleton,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
+} from "@/components/ui/sidebar-menu"
---
+++ src/components/views.tsx
@@ -0,0 +1,6 @@
+export { TodayView } from "./views-today";
+export { LibraryView } from "./views-library";
+export { EditView } from "./views-edit";
+export { PickView } from "./views-pick";
+export { PublishingView } from "./views-publishing";
+export { PerformanceView } from "./views-performance";
